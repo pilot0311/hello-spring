e02ccf29559a0a8ae6a13e9987f39a19ff76028f
pilot0311 <pilot0938@gmail.com>
2022-11-27 22:17:28 +0900

    test 2

---
+++ src/main/java/hello/hello/spring/service/MemberService.java
@@ -0,0 +1,45 @@
+package hello.hello.spring.service;
+
+import hello.hello.spring.domain.Member;
+import hello.hello.spring.repository.MemberRepository;
+import hello.hello.spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        //중복회원 안됨
+        validateDuplicateMember(member); //중복회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findByAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
